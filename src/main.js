59a04feecec1a64065e7f3f8f81f0b5fde7ddb3e
qwinsi <70425035+qwinsi@users.noreply.github.com>
2024-08-25 12:05:08 +0800

    keybinding to quit the app

--- src/main.js
+++ src/main.js
@@ -2,4 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 
+import { exit } from '@tauri-apps/api/process'
+import { platform } from '@tauri-apps/api/os'
+
 createApp(App).mount("#app");
+
+
+// Ctrl+Q on Windows/Linux or Cmd+Q on macOS to quit the app
+platform().then(platformName => {
+    if (platformName === 'win32' || platformName === 'linux') {
+        window.addEventListener('keydown', function (e) {
+            if (e.ctrlKey && e.key === 'q') {
+                exit(0);
+            }
+        });
+    } else if (platformName === 'darwin') {
+        window.addEventListener('keydown', function (e) {
+            if (e.metaKey && e.key === 'q') {
+                exit(0);
+            }
+        });
+    }
+});
