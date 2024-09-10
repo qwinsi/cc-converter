48389d23e410c73cf521f6473ac176e72d1331ba
qwinsi <70425035+qwinsi@users.noreply.github.com>
2024-09-11 07:44:10 +0800

    single instance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{AppHandle, Manager};
 use zhconv::{zhconv, Variant};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn tc2sc(input: &str) -> String {
-    return zhconv(input, Variant::ZhHans)
+    return zhconv(input, Variant::ZhHans);
 }
 
 #[tauri::command]
 fn sc2tc(input: &str) -> String {
-    return zhconv(input, Variant::ZhHant)
+    return zhconv(input, Variant::ZhHant);
 }
 
 #[tauri::command]
@@ -30,9 +32,30 @@ fn quit(code: i32) {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            let _ = show_window(app);
+        }))
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![tc2sc, sc2tc, tc2sc_phrase, sc2tc_phrase, quit])
+        .invoke_handler(tauri::generate_handler![
+            tc2sc,
+            sc2tc,
+            tc2sc_phrase,
+            sc2tc_phrase,
+            quit
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// https://v2.tauri.app/plugin/single-instance/
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
+
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
 }
